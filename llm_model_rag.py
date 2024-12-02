9e22091ebd6d9ba5a44e95a24995361fcc84e366
seok0205 <9526yu@naver.com>
2024-12-02 11:42:20 +0900

    update llm_model_rag.py
    모듈화 및 코드 수정

--- llm_model_rag.py
+++ llm_model_rag.py
@@ -12,6 +12,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from operator import itemgetter
 from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # api key, env파일에서 로드.
 load_dotenv(dotenv_path='key.env')
@@ -54,7 +55,9 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 split_documents = text_splitter.split_documents(data)
 
 # 임베딩 생성 및 벡터 DB 생성, 저장
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+# embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# BM25, hybrid search을 활용하여 성능 개선 가능.
 vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
 
 # 리트리버(검색기) 생성. 문서의 정보 검색 및 생성
@@ -82,20 +85,25 @@ prompt = PromptTemplate.from_template(
 """
 )
 
-# llm모델 gpt-4o으로 생성.
-llm = ChatOpenAI(model="gpt-4o", temperature=0.3)
+def create_chain(retriever, prompt):
+    # llm모델 gpt-4o으로 생성.
+    llm = ChatOpenAI(model="gpt-4o", temperature=0.3)
 
-# 체인 생성
-chain = (
-    {
-        "info": itemgetter("question") | retriever,
-        "question": itemgetter("question"),
-        "chat_history": itemgetter("chat_history"),
-    }
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+    # 체인 생성
+    chain = (
+        {
+            "info": itemgetter("question") | retriever,
+            "question": itemgetter("question"),
+            "chat_history": itemgetter("chat_history"),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return chain
+
+chain = create_chain(retriever, prompt)
 
 # 대화 세션 기록
 chat = {}
@@ -108,22 +116,32 @@ def get_session_history(session_ids):
         chat[session_ids] = ChatMessageHistory()
     return chat[session_ids]
 
-rag_with_chat = RunnableWithMessageHistory(
-    chain,
-    get_session_history,  # 세션 기록을 가져오는 함수
-    input_messages_key="question",  # 사용자의 질문이 템플릿 변수에 들어갈 key
-    history_messages_key="chat_history",  # 기록 메시지의 키
-)
+def create_rag_with_chat(chain):
+    '''RunnableWithMessageHistory 생성'''
+    rag_with_chat = RunnableWithMessageHistory(
+        chain,
+        get_session_history,  # 세션 기록을 가져오는 함수
+        input_messages_key="question",  # 사용자의 질문이 템플릿 변수에 들어갈 key
+        history_messages_key="chat_history",  # 기록 메시지의 키
+    )
+    return rag_with_chat
+
+rag_with_chat = create_rag_with_chat(chain)
 
 # 코드 테스트용.
 if __name__ == "__main__":
-    user_input = input("질문 입력하세요: ")
-    print("result: ", rag_with_chat.invoke(
-    {"question": user_input},
-    config={"configurable": {"session_id": "jungseok"}})
-    )
-    user_input_2 = input("질문 입력하세요: ")
-    print("result: ", rag_with_chat.invoke(
-    {"question": user_input_2},
-    config={"configurable": {"session_id": "jungseok"}})
-    )
+    while True:
+        session_id = "jungseok"
+        question = input("질문 입력하세요: ")
+        if question == "exit":
+            break
+        
+        response = rag_with_chat.invoke(
+            {
+            "question": question,
+            "session_ids": session_id
+            },
+        config={"configurable": {"session_id": session_id}}
+        )
+
+        print(f"답: {response}")
